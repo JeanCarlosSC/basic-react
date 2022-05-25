6ef3593dc50b7f40bda2ee1c9142e8f1a05f9250
JeanCarlosSC <jeancarlosodi@gmail.com>
2022-05-25 16:10:15 -0500

    adding incomplete files hello-world and tic-ta-toe

---
+++ 00.hello-world/Main.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.__esModule = true;
+var ReactDOM = require("react-dom");
+var root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<h1>Hello, world!</h1>);
