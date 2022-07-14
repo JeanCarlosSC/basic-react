1a7eb3baeb090cf52e2f01e791c31a06ef1536d3
JeanCarlosSC <jeancarlosodi@gmail.com>
2022-07-14 17:11:04 -0500

    first step for create a history

--- practice/01.tic-tac-toe/src/index.js
+++ practice/01.tic-tac-toe/src/index.js
@@ -78,18 +78,29 @@ class Board extends React.Component {
   }
 }
 
-function Game(props) {
-  return (
-    <div className="game">
-      <div className="game-board">
-        <Board />
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null),
+      }],
+    }
+  }
+
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
       </div>
-      <div className="game-info">
-        <div>{/* status */}</div>
-        <ol>{/* TODO */}</ol>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 function calculateWinner(squares) {
