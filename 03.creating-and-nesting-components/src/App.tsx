ffe4e0df6dd147cdd3a90daaecd1ca30389d2e7a
JeanCarlosSC <jeancarlosodi@gmail.com>
2024-05-25 13:26:56 -0500

    Creating and nesting components

---
+++ 03.creating-and-nesting-components/src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to my app!</h1>
+      <MyButton />
+    </div>
+  );
+}
+
+function MyButton() {
+  return (
+    <button>I'm a button</button>
+  )
+}
+
+export default App;
