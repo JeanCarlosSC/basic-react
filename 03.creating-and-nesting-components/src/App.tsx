e635b5694d692c321214bbc7b6100cd546e72e83
JeanCarlosSC <jeancarlosodi@gmail.com>
2024-05-25 13:36:05 -0500

    Adding styles

--- 03.creating-and-nesting-components/src/App.tsx
+++ 03.creating-and-nesting-components/src/App.tsx
@@ -2,10 +2,11 @@ import './App.css';
 
 function App() {
   return (
-    <>
+    <div className="App">
       <h1>Welcome to my app!</h1>
+      <p>Made by Jean Carlos SC.</p>
       <MyButton />
-    </>
+    </div>
   );
 }
 
