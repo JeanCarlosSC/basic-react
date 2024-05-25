110f015cc7a7229503cd1f01df1dae8bf3450c0d
JeanCarlosSC <jeancarlosodi@gmail.com>
2024-05-25 13:44:51 -0500

    Displaying data

--- 03.creating-and-nesting-components/src/App.tsx
+++ 03.creating-and-nesting-components/src/App.tsx
@@ -1,19 +1,34 @@
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Welcome to my app!</h1>
-      <p>Made by Jean Carlos SC.</p>
-      <MyButton />
-    </div>
-  );
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
 }
 
 function MyButton() {
   return (
     <button>I'm a button</button>
   )
+}
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to my app!</h1>
+      <img
+        src={user.imageUrl}
+        alt={'Photo of:' + user.name} 
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+      <p>{user.name}</p>
+      
+      <MyButton />
+    </div>
+  );
 }
 
 export default App;
